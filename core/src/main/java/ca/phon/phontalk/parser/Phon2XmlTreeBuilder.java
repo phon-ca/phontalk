de8627a31ed691eeb7c9b231f8fa0b1bc5a4a7ec
Greg Hedlund <greg.hedlund@gmail.com>
2020-09-09 08:14:37 -0230

    Update CHAT element

--- core/src/main/java/ca/phon/phontalk/parser/Phon2XmlTreeBuilder.java
+++ core/src/main/java/ca/phon/phontalk/parser/Phon2XmlTreeBuilder.java
@@ -42,7 +42,7 @@ public class Phon2XmlTreeBuilder {
 	
 	private final static Logger LOGGER = Logger.getLogger(Phon2XmlTreeBuilder.class.getName());
 	
-	private final static String TALKBANK_VERSION = "2.10.0";
+	private final static String TALKBANK_VERSION = "2.16.0";
 	
 	private static final AntlrTokens talkbankTokens = new AntlrTokens("TalkBank2AST.tokens");
 	
