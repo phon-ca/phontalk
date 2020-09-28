ca08783c55f66cc7353ad7072d7f19c8bb2f33ab
Greg Hedlund <greg.hedlund@gmail.com>
2020-09-28 16:58:53 -0230

    Allow override of chatter location

--- core/src/main/java/ca/phon/phontalk/CHATRunner.java
+++ core/src/main/java/ca/phon/phontalk/CHATRunner.java
@@ -4,10 +4,12 @@ import java.io.*;
 import java.net.*;
 
 import ca.phon.util.OSInfo;
+import ca.phon.util.PrefHelper;
 
 public class CHATRunner {
 
-	private final static String CHATTER_JAR = "bin/chatter.jar";
+	public final static String CHATTER_PROP = CHATRunner.class.getName() + ".chatter";
+	private final static String DEFAULT_CHATTER_JAR = "bin/chatter.jar";
 	
 	public CHATRunner() {
 	}
@@ -18,15 +20,19 @@ public class CHATRunner {
 		final String javaBin = javaHome + File.separator + "bin" + File.separator + "java" + 
 				(OSInfo.isWindows() ? ".exe" : "");
 		
-		final URL chatterURL = ClassLoader.getSystemResource(CHATTER_JAR);
-		String chatter = CHATTER_JAR;
-		if(chatterURL != null) {
-			try {
-				chatter = new File(chatterURL.toURI()).getAbsolutePath();
-			} catch (URISyntaxException e) {
-				throw new IOException(e);
+		final String chatterLoc = PrefHelper.get(CHATTER_PROP, DEFAULT_CHATTER_JAR);
+		File chatterFile = new File(chatterLoc);
+		String chatter = chatterFile.getAbsolutePath();
+		if(!chatterFile.isAbsolute()) {
+			final URL chatterURL = ClassLoader.getSystemResource(chatterLoc);
+			if(chatterURL != null) {
+				try {
+					chatter = new File(chatterURL.toURI()).getAbsolutePath();
+				} catch (URISyntaxException e) {
+					throw new IOException(e);
+				}
 			}
-		}
+		}			
 		
 		final ProcessBuilder processBuilder = new ProcessBuilder(javaBin,
 						"-cp", chatter, 
