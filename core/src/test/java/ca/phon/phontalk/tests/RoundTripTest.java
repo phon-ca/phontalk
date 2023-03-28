d894217f30590cd6eb7fa25a9fce4163da3dbdb5
Greg Hedlund <greg.hedlund@gmail.com>
2023-03-28 08:06:01 -0230

    Add support for importing wor internal-media elements into new Group Segment tier

---
+++ core/src/test/java/ca/phon/phontalk/tests/RoundTripTest.java
@@ -0,0 +1,19 @@
+package ca.phon.phontalk.tests;
+
+/**
+ * Test round trip: cha -> xml -> phon -> xml -> cha
+ *
+ */
+public class RoundTripTest {
+
+    private String inputFile;
+
+    private String outputFile;
+
+    public RoundTripTest(String inputFile, String outputFile) {
+        super();
+    }
+
+
+
+}
