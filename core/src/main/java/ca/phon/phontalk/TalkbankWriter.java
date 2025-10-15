162facb891326f42fa86d08eea93be7ffe2d414b
Gregory Hedlund <greg.hedlund@gmail.com>
2025-10-15 12:40:37 -0230

    bump chatter and schema versions

--- core/src/main/java/ca/phon/phontalk/TalkbankWriter.java
+++ core/src/main/java/ca/phon/phontalk/TalkbankWriter.java
@@ -37,7 +37,7 @@ public class TalkbankWriter {
 
     private final static String TBNS_SCHEMA_LOCATION = "http://www.talkbank.org/ns/talkbank https://talkbank.org/software/talkbank.xsd";
 
-    private final static String TB_VERSION = "3.2.1";
+    private final static String TB_VERSION = "3.2.2";
 
     private final List<PhonTalkListener> listeners = new ArrayList<>();
 
